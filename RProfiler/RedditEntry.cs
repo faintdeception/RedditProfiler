94cd0ecd08445717a8ffd77f01b6379df0a4a6e6
Zach Ruffin <zachruffin@gmail.com>
2011-06-24 20:18:27 -0400

    Adding some helpful code to get the data from yahoo pipes.

---
+++ RProfiler/RedditEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel;
+
+namespace RProfiler
+{
+  [DataContract]
+  public class RedditEntry
+  {
+    /// <summary>
+    /// Gets or sets the title of the reddit post.
+    /// </summary>
+    [DataMember(Name="title")]
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Gets or sets the thumbnail image.
+    /// </summary>
+    //[DataMember(Name="thumbnail")]
+    //public string Thumbnail { get; set; }
+
+    
+    /// <summary>
+    /// Gets or sets the url to actual contents.
+    /// </summary>
+    [DataMember(Name="link")]
+    public string Link { get; set; }
+
+    /// <summary>
+    /// Gets or sets the number of comments.
+    /// </summary>
+    //[DataMember(Name="num_comments")]
+    //public int NumComments { get; set; }
+  }
+}
